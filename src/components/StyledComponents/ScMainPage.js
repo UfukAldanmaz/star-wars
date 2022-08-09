5d6d6385255ebeaf979ec148365db558c0e71c52
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-10 00:26:46 +0300

    Add style

--- src/components/StyledComponents/ScMainPage.js
+++ src/components/StyledComponents/ScMainPage.js
@@ -1,8 +1,45 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+
+background: black;
+color: yellow;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10%;
+  .header {
+    color: yellow;
+  }
+  .search{
+    background-color: purple;
+    width: 350px;
+    margin-right: 15px;
+    padding: 5px;
+    border-radius: 12px;
+    border:none;&:hover{
+    width: 370px;
+    box-shadow: 1px 2px yellow;
+  }
+  }
+  .search::placeholder{
+    color: yellow;
+  }
+  .selection{
+    padding: 5px;
+    background-color: purple;
+    color: yellow;
+    border-radius: 12px;
+    border:none;
+    cursor: pointer;
+    &:hover {
+      width: 110px;
+    box-shadow: 3px 3px yellow;
+    }
+  }.input-drop-container {
+    display: flex;
+    flex-direction:row;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 20px;   
+  }
 `
--- src/components/StyledComponents/ScPeople.js
+++ src/components/StyledComponents/ScPeople.js
@@ -1,5 +1,72 @@
 import styled from "styled-components";
 
 export const PeopleWrapper = styled.div`
+backround-color: red;
+margin-top: 20px;
+padding: 0;
 
+.titles{
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    border: 1px solid gray;
+    border-radius: 8px;
+    height: 50px;
+    margin:0 5px 0 5px;
+  
+  
+  }
+  .table-wrap {
+    margin-bottom: 50px;
+
+    
+
+  }
+  .table-title{
+    text-align:center;
+    padding:15px;
+    width:190px;
+  }
+  .table-title-act{
+    margin-top: 15px;
+        margin-left: 35px;
+        width: 80px
+  }.people{
+    display: flex;
+    align-items:center;
+    flex-direction: row;
+    height: 50px;
+    border: 1px solid gray;
+    margin: 5px;
+    
+    padding-bottom: 25px;
+    border-radius: 8px;
+ 
+  }.remove-btn {
+    background-color: inherit;
+    float: right;
+    font-size: 10px;
+    border: 1px solid gray;
+    width: 45px;
+    padding: 8px;
+    border-radius: 5px;
+    margin-right: 35px;
+  }
+  .people-table{ 
+   width: 180px;
+   text-align: center;
+   margin: 30px 5px 5px 25px;
+    }.people-table-btn{
+        margin-top: 30px;
+        margin-left: 50px;
+        width: 80px
+    }
+    .people-table-films {
+        overflow: scroll;
+        border: 1px solid gray;
+        width: 250px;
+        height: 60px;
+       margin-top: 25px;
+    }
 `
+
