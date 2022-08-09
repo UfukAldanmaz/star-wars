f88791db6014c2761589cf7c0e735c46822d81af
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-09 11:32:56 +0300

    Add style

---
+++ src/components/StyledComponents/ScLogin.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledLogin = styled.div`
+ display: flex;
+  flex-direction: column;
+  align-items: center;`
+
+export const Input = styled.input`
+padding: 5px;
+margin: 5px;
+border: 1px solid blue;`
