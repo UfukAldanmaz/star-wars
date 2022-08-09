c62c0a7c04dc4210f63d0a2133ddda1548c0828a
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-10 00:26:27 +0300

    Update PeopleList.jsx

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -4,15 +4,33 @@ import PeopleCard from "./PeopleCards";
 
 
 class PeopleList extends React.Component {
+    deletePerson = (e) => {
+        e.target.parentElement.remove();
+    }
     render() {
         return (
             <div className="people-list">
                 <PeopleWrapper>
-                    {this.props.data.map(item => (
-                        <div key={item.name}><PeopleCard item={item} />
-                            <button onClick={this.props.delete}>X</button>
-                        </div>
-                    ))}
+                    <table className="table-wrap">
+                        <thead>
+                            <tr className="titles">
+                                <th className="table-title">Name</th>
+                                <th className="table-title">Height</th>
+                                <th className="table-title">Gender</th>
+                                <th className="table-title">Films</th>
+                                <th className="table-title-act">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.data.map(item => (
+                                <tr className="people" key={item.name}>
+                                    <PeopleCard item={item} />
+                                    <td className="people-table-btn"><button onClick={this.deletePerson} className='remove-btn'>❌</button></td>
+                                </tr>
+                            ))}
+
+                        </tbody>
+                    </table>
                 </PeopleWrapper>
             </div>
         )
