3cb37f10b3ac2c467e787b8c3808f2e391d3a93a
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-10 12:42:48 +0300

    Update ScLogin.js

--- src/components/StyledComponents/ScLogin.js
+++ src/components/StyledComponents/ScLogin.js
@@ -1,11 +1,42 @@
 import styled from 'styled-components';
 
 export const StyledLogin = styled.div`
- display: flex;
-  flex-direction: column;
-  align-items: center;`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-export const Input = styled.input`
-padding: 5px;
-margin: 5px;
-border: 1px solid blue;`
+  .login-btn{
+    background-color: purple;
+    color: yellow;
+    width:70px;
+    border-radius: 12px;
+    border: none;
+    margin-top: 25px;
+    cursor: pointer;
+    height: 35px;
+  }
+  .name-input{
+    width: 200px;
+    border-radius: 12px;
+    padding: 10px;
+    margin-top: 25px;
+    border: none;
+
+    background-color: purple;&::placeholder{
+    color:yellow;
+  }
+  }
+  .pass-input{
+    width: 200px;
+    border-radius: 12px;
+    padding: 10px;
+    margin-top: 25px;
+    border: none;
+    color: yellow;
+    background-color: purple;&::placeholder
+    {
+    color:yellow;
+  }
+  }
+  `
+
