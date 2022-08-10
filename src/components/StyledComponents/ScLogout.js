1a512522f903e188eb45df6555ba9e886c9e658c
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-11 00:39:48 +0300

    Update ScLogout.js

--- src/components/StyledComponents/ScLogout.js
+++ src/components/StyledComponents/ScLogout.js
@@ -4,14 +4,14 @@ export const StyledLogout = styled.div`
 .logout{
     background-color: purple;
     color: yellow;
-    width: 55px;
-    padding: 5px;
+    width: 60px;
+    padding: 7px;
     margin-bottom: 10px;
     border-radius: 10px;
     border: none;
     cursor: pointer;&:hover{
         box-shadow: 2px 2px yellow;
-        width: 60px;
+        width: 65px;
     }
 }
 `
