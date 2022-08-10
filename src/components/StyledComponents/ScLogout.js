85b6bb1933d594c1083f7c01a64414e41d701539
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-10 12:42:56 +0300

    Create ScLogout.js

---
+++ src/components/StyledComponents/ScLogout.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledLogout = styled.div`
+.logout{
+    background-color: purple;
+    color: yellow;
+    width: 55px;
+    padding: 5px;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+}
+`
